aa5de44101615e656b33385eb58bb6583fd5b192
Logan <logancable@ymail.com>
2023-02-06 13:04:52 -0800

    second commit, added functionality to color in grid and clear grid

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 // Your code here
+let table = document.getElementsByTagName('table')[0];
+let button = document.getElementById('add-row');
+let select = document.getElementsByTagName('select')[0];
+let clearButton = document.getElementById('clear');
+let selectedColor = "red";
+
+
+function makeRow () {
+    let row = document.createElement('tr');
+    for (let i = 0; i < 20; i++) {
+        let td = document.createElement('td');
+        row.appendChild(td);
+    }
+    table.appendChild(row)
+}
+
+function colorize (event) {
+    if (event.target.tagName === "TD") {
+        let target = event.target
+        if (target.className.length) {
+            target.className = ''
+        } else {
+            target.className = selectedColor;
+        }
+    }
+}
+
+function check (event) {
+    console.log(event.target.tagName)
+}
+
+function clearBoard() {
+    let cells = table.getElementsByTagName('td');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].className = '';
+    }
+}
+
+
+
+button.addEventListener('click', makeRow);
+table.addEventListener('mouseover', colorize);
+
+select.addEventListener('change', function (event) {
+    selectedColor = event.target.value
+});
+
+clearButton.addEventListener('click', clearBoard);
+
